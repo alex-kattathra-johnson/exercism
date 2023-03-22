0b8b1e5b97782e181217f486e20d536040f5456f
Alex Johnson <ajohnson@bombora.com>
2023-03-22 18:06:21 -0400

    Update to use where clause
    Signed-off-by: Alex Johnson <ajohnson@bombora.com>

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -9,20 +9,18 @@ data Planet = Mercury
             | Uranus
             | Neptune
 
-earthYears :: Float -> Float
-earthYears seconds = seconds / 31557600
-
-orbitalPeriod :: Planet -> Float
-orbitalPeriod planet =
-    case planet of
-        Mercury -> 0.2408467
-        Venus -> 0.61519726
-        Earth -> 1.0
-        Mars -> 1.8808158
-        Jupiter -> 11.862615
-        Saturn -> 29.447498
-        Uranus -> 84.016846
-        Neptune -> 164.79132
-
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = earthYears seconds / orbitalPeriod planet
+ageOn planet seconds = seconds / (periodInEarthYears * secondsPerEarthYear)
+  where
+    secondsPerEarthYear = 60 * 60 * 24 * 365.25
+
+    periodInEarthYears = case planet of
+      Mercury -> 0.2408467
+      Venus -> 0.61519726
+      Earth -> 1
+      Mars -> 1.8808158
+      Jupiter -> 11.862615
+      Saturn -> 29.447498
+      Uranus -> 84.016846
+      Neptune -> 164.79132
+
