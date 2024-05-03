e7edcff4a8348241f2b8bd488beaa6fdbf45a0ce
Alex Johnson <hello@alex-johnson.net>
2024-05-03 16:05:27 -0500

    Nucleotide count exercise
    Signed-off-by: Alex Johnson <hello@alex-johnson.net>

---
+++ haskell/nucleotide-count/src/DNA.hs
@@ -0,0 +1,17 @@
+module DNA (nucleotideCounts, Nucleotide(..)) where
+
+import Data.Char (toUpper)
+import Data.Map (Map)
+import qualified Data.Map as M
+
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
+
+nucleotideCounts :: String -> Either String (Map Nucleotide Int)
+nucleotideCounts xs
+  | isValid xs = Right $ count xs
+  | otherwise = Left xs
+  where
+    isValid = all (`elem` "ACGT")
+
+count :: String -> Map Nucleotide Int
+count xs = M.fromListWith (+) [(read [toUpper c], 1) | c <- xs]
