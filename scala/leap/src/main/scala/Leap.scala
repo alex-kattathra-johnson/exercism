41265b712283ebf443dffd183eeacbf15287b220
Alex Johnson <hello@alex-johnson.net>
2024-05-09 07:16:46 -0500

    Complete first java challenge
    Signed-off-by: Alex Johnson <hello@alex-johnson.net>

---
+++ scala/leap/src/main/scala/Leap.scala
@@ -0,0 +1,10 @@
+object Leap {
+  def leapYear(year: Int): Boolean = {
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+       return true
+    }
+    else{
+      return false
+    }
+  }
+}
