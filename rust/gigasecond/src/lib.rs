6a8dddca591ff6b8b0749e9b6c75fe5dc945f006
Alex Johnson <alex.kattathra.johnson@gmail.com>
2024-10-22 11:52:04 -0500

    Gigasecond exercise in rust
    Signed-off-by: Alex Johnson <alex.kattathra.johnson@gmail.com>

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, Duration, Utc};
 
 pub fn after(time: DateTime<Utc>) -> DateTime<Utc> {
-    return time + Duration::seconds(1_000_000_000);
+    time + Duration::seconds(1_000_000_000)
 }
