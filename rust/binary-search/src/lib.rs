fa5a41af1d450c7bff850770007e7ffae5119f88
Alex Johnson <alex.kattathra.johnson@gmail.com>
2024-10-26 19:24:28 -0500

    Complete binary search exercise
    Signed-off-by: Alex Johnson <alex.kattathra.johnson@gmail.com>

---
+++ rust/binary-search/src/lib.rs
@@ -0,0 +1,11 @@
+use core::cmp::Ordering;
+
+pub fn find<R: AsRef<[T]>, T: Ord>(array: R, key: T) -> Option<usize> {
+    let array = array.as_ref();
+    let mid = array.len()/2;
+    match key.cmp(array.get(mid)?) {
+        Ordering::Equal => Some(mid),
+        Ordering::Less => find(&array[..mid], key),
+        Ordering::Greater => find(&array[mid+1..], key).map(|i| i + mid + 1)
+    }
+}
